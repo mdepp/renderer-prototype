675aca6bbca3202ce526a5861a533b2e3f504ca1
mdepp <mdepp@ualberta.ca>
2020-02-14 18:06:20 -0700

    Create application window class

---
+++ src/ApplicationWindow.cpp
@@ -0,0 +1,37 @@
+#include "ApplicationWindow.hpp"
+
+app::ApplicationWindow::ApplicationWindow(const size_t width, const size_t height)
+        : m_width(width), m_height(height), m_window(nullptr), m_renderer(nullptr), m_screen_buffer(width, height) {
+    SDL_CreateWindowAndRenderer(width, height, SDL_WINDOW_SHOWN, &m_window, &m_renderer);
+    if (!m_window || !m_renderer) {
+        throw SDLException("Failed to create SDL window and renderer");
+    }
+    m_screen_texture = SDL_CreateTexture(m_renderer, SDL_PIXELFORMAT_RGB888, SDL_TEXTUREACCESS_STREAMING, m_width, m_height);
+    if (!m_screen_texture) {
+        throw SDLException("Failed to create buffer texture");
+    }
+}
+
+void app::ApplicationWindow::draw(const buffer::Buffer<glm::vec3> &texture) {
+    SDL_PixelFormat pixel_format;
+    pixel_format.format = SDL_GetWindowPixelFormat(m_window);
+    texture.for_each_pixel([&](const glm::ivec2& position) {
+        const auto colour = texture.at(position);
+        const uint32_t r = colour.r * 255;
+        const uint32_t g = colour.g * 255;
+        const uint32_t b = colour.b * 255;
+        auto val = (r << 16) + (g << 8) + b;
+        m_screen_buffer.at(glm::ivec2(position.x, m_height-1-position.y)) = val;
+    });
+    SDL_UpdateTexture(m_screen_texture, nullptr, m_screen_buffer.data(), sizeof(uint32_t)*m_width);
+    SDL_RenderCopy(m_renderer, m_screen_texture, nullptr, nullptr);
+    SDL_RenderPresent(m_renderer);
+}
+
+app::SDLException::SDLException(const std::string &message) {
+    m_message = message + std::string(": ") + SDL_GetError();
+}
+
+const char *app::SDLException::what() const noexcept {
+    return m_message.c_str();
+}
---
+++ src/ApplicationWindow.hpp
@@ -0,0 +1,56 @@
+#ifndef RENDERER_PROTOTYPE_APPLICATIONWINDOW_HPP
+#define RENDERER_PROTOTYPE_APPLICATIONWINDOW_HPP
+
+#include <SDL.h>
+#include <exception>
+#include <string>
+#include "Buffer.hpp"
+#include <algorithm>
+
+namespace app {
+
+    class SDLException : public std::exception {
+    public:
+        explicit SDLException(const std::string& message);
+
+        const char* what() const noexcept override;
+
+    private:
+        std::string m_message;
+    };
+
+    class ApplicationWindow {
+    public:
+        explicit ApplicationWindow(size_t width, size_t height);
+        ApplicationWindow(const ApplicationWindow&) = delete;
+        ApplicationWindow& operator = (const ApplicationWindow&) = delete;
+
+        void draw(const buffer::Buffer<glm::vec3>& texture);
+
+        bool poll_events() {
+            SDL_Event event;
+            while (SDL_PollEvent(&event)) {
+                if (event.type == SDL_QUIT) return false;
+            }
+            return true;
+        }
+
+        virtual ~ApplicationWindow() {
+            SDL_DestroyTexture(m_screen_texture);
+            SDL_DestroyRenderer(m_renderer);
+            SDL_DestroyWindow(m_window);
+        }
+
+    private:
+        const size_t m_width;
+        const size_t m_height;
+        SDL_Window* m_window;
+        SDL_Renderer* m_renderer;
+        buffer::Buffer<uint32_t> m_screen_buffer;
+        SDL_Texture* m_screen_texture;
+    };
+
+}
+
+
+#endif //RENDERER_PROTOTYPE_APPLICATIONWINDOW_HPP
